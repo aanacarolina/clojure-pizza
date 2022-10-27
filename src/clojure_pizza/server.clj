70abba4d65a3ff9fb6bfad817c178b88b9cb18c2
CarolSN <79257314+aanacarolina@users.noreply.github.com>
2022-10-27 13:44:34 -0300

    config server

--- src/clojure_pizza/server.clj
+++ src/clojure_pizza/server.clj
@@ -1,1 +1,14 @@
-(ns clojure-pizza.server)
+(ns clojure-pizza.server
+ (:require [io.pedestal.http :as http]))
+
+(defonce server (atom nil))
+
+(defn start-server [service-map]
+  (reset! server (http/start (http/create-server service-map))))
+
+(defn stop-server []
+  (http/stop @server))
+
+(defn restart-server [service-map]
+  (stop-server)
+  (start-server service-map))
